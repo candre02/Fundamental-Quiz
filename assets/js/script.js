9b27e59ed965a3c339aa3fc7d856ec636a0fb4d3
Crystal Andre <81876258+candre02@users.noreply.github.com>
2021-10-13 10:50:53 -0500

    added js required elements, start quiz button, end button, continue btn, added infobox.activeinfo in style.css, added outline property in infobox btn button, added infobox.activequiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,22 @@
 // getting all required element
-const start_btn = document.querySelector("")
+const start_btn = document.querySelector(".start_btn button");
+const info_box = document.querySelector(".info_box");
+const  end_btn = info_box.querySelector(".buttons .end");
+const continue_btn = info_box.querySelector(".buttons .restart");
+const quiz_box = document.querySelector(".quiz_box");
+
+// If start quiz button clicked
+start_btn.onclick = () {
+    info_box.classList.add("activeInfo");  // show the info box 
+}
+
+// If end button clicked
+end_btn.onclick = () {
+    info_box.classList.remove("activeInfo");  // hide the info box
+}
+
+// If Continue button clicked
+continue_btn.onclick = () {
+    info_box.classList.remove("activeInfo");  // hide the info box
+    quiz_box.classList.add("activeQuiz");  // show the quiz box
+}
