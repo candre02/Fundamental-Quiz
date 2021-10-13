a873bee2e154934b9dc120287dde5a805adcd834
Crystal Andre <81876258+candre02@users.noreply.github.com>
2021-10-13 14:29:37 -0500

    added option.correct in css, created a questions.js file, inputed all questions in the ques.js file, added if else inside the next btn section in script.js, added q's and options array in js, added two functions inside Q's and options array section line 48 and 66, inside func option selected added let element, if else statement, answer.correct and answer.incorrect, add option.correct and option.incorrect in css, selected disabled added a for loop, added option.disabled in css, disabled option added if statement, let tick and cross icon at lines 63-64, line 7 added timercount, fixed line 80-83 mixed up content, line 89 was missing content, added let counter at line 31

---
+++ assets/js/question.js
@@ -0,0 +1,58 @@
+// craeting an array and passing the number, questions, options, and answers
+let questions = [
+    {
+        numb: 1.,
+        question: "What does DOM stand for?",
+        answer: "Documnet Object Model",
+        option: [
+            "Documnet Object Model",
+            "Documnet Order Market",
+            "Days Of Market",
+            "Doctor Oriental Medicine"
+        ]
+    },
+    {
+        numb: 2.,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        option: [
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language"
+        ]
+    },
+    {
+        numb: 3.,
+        question: "What function is passed as an arguement to another function?",
+        answer: "CallBack()",
+        option: [
+            "indexOf()",
+            "Math.floor()",
+            "Push()",
+            "CallBack()"
+        ]
+    },
+    {
+        numb: 4.,
+        question: "What method can be used to Add or Update any Attribute on an HTML element?",
+        answer: "setAttribute()",
+        option: [
+            "setAttribute()",
+            "replace()",
+            "slice()",
+            "charCodeAt()"
+        ]
+    },
+    {
+        numb: 5.,
+        question: "What does CSS stand for?",
+        answer: "Cascading Style Sheets",
+        option: [
+            "Capital Structure Substitution Theory",
+            "Certified Coding Specialist",
+            "Cascading Style Sheets",
+            "Customer Service Support"
+        ]
+    }
+]
--- assets/js/script.js
+++ assets/js/script.js
@@ -4,19 +4,104 @@ const info_box = document.querySelector(".info_box");
 const  end_btn = info_box.querySelector(".buttons .end");
 const continue_btn = info_box.querySelector(".buttons .restart");
 const quiz_box = document.querySelector(".quiz_box");
+const timeCount = quiz_box.querySelector(".timer .timer_sec");
+
+const option_list = document.querySelector(".option_list");
 
 // If start quiz button clicked
-start_btn.onclick = () {
+start_btn.onclick = () => {
     info_box.classList.add("activeInfo");  // show the info box 
 }
 
 // If end button clicked
-end_btn.onclick = () {
+end_btn.onclick = () => {
     info_box.classList.remove("activeInfo");  // hide the info box
 }
 
 // If Continue button clicked
-continue_btn.onclick = () {
+continue_btn.onclick = () => {
     info_box.classList.remove("activeInfo");  // hide the info box
     quiz_box.classList.add("activeQuiz");  // show the quiz box
+    showQuestions(0);
+    queCounter(1);
+}
+
+let que_count = 0;
+let que_numb = 1;
+let counter;
+
+const next_btn = quiz_box.querySelector(".next_btn");
+
+// If Next button clicked
+next_btn.onclick = () => {
+    if (que_count < questions.length - 1) {
+        que_count++;
+        que_numb++;
+        showQuestions(que_count);
+        queCounter(que_numb);
+    } else {
+        console.log("Questions completed");
+    }
+}
+
+// getting questions and options from array
+function showQuestions(index) {
+    const question_text = document.querySelector(".question_text");
+    let question_tag = '<span>'+ questions[index].numb + ". " + questions[index].question +'</span>';
+    let option_tag = '<div class="option">'+ questions[index].option[0] +'<span></span></div>';
+                     + '<div class="option">'+ questions[index].options[1] +'<span></span></div>'
+                     + '<div class="option">'+ questions[index].options[2] +'<span></span></div>'
+                     + '<div class="option">'+ questions[index].options[3] +'<span></span></div>';
+    question_text.innerHTML = question_tag;
+    option_list.innerHTML = option_tag;
+    const option = option_list.querySelectorAll(".option");
+    for (let index = 0; i < option.length; i++) {
+        option[i].setAttribute("onclick", "optionSelected(this)");
+    }                 
+}
+
+let tickIcon = '<div class="icon tick"><i class="fas fa-check"></i></div>';
+let crossIcon = '<div class="icon cross"><i class="fas fa-times"></i></div>';
+
+function optionSelected(answer) {
+    let userAns = answer.textContent;
+    let correctAns = questions[que_count].answer;
+    let allOptions = option_list.children.length;
+    if (userAns === correctAns) {
+        answer.classList.add("correct");
+        console.log("Answer is Correct");
+        answer.insertAdjacentHTML("beforeend", tickIcon);
+    } else {
+        answer.classList.add("incorrect");
+        console.log("Answer is Wrong");
+        answer.insertAdjacentHTML("beforeend", crossIcon);
+        
+        // if answers is incorrect the automatically selected the correct answer
+        for (let i = 0; i < allOptions; i++) {
+            if (option_list.children[i].textContent === correctAns) {
+                option_list.children[i].setAttribute("class", "option correct");
+                option_list.children[i].insertAdjacentHTML("beforeend", tickIcon);
+            }
+        }
+    }
+
+    // once user selected disabled all options
+    for (let i = 0; i < allOptions.length; i++) {
+        option_list.children[i].classList.add("disabled");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+function queCounter(index) {
+    const bottom_ques_counter = quiz_box.querySelector(".total_question");
+    let totalQuestionCountTag = '<span><p>'+ index +'</p>of<p>'+ questions.length +'</p>Questions</span>';
+    bottom_ques_counter.innerHTML = totalQuestionCountTag;
 }
